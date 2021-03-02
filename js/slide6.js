c1dd74a9879a4d0a6b3211d2b0de6afec42d0d39
ss-hyon <so.young.ee197@gmail.com>
2021-03-01 22:49:55 -0800

    35485

--- js/slide6.js
+++ js/slide6.js
@@ -4,3 +4,15 @@
  *
  * Public Domain
  **/
+ //text box info
+$("#mk").hide();
+
+ function marketInfo() {
+  var x = document.getElementById("mk");
+  $("x").hide();
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
