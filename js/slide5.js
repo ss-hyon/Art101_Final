c294273a1d7f3deb48abc956cbc5bcbe4a061ffc
Yuwei-Mao <77122567+Yuwei-Mao@users.noreply.github.com>
2021-03-01 22:54:18 -0800

    Update slide5.js

--- js/slide5.js
+++ js/slide5.js
@@ -7,15 +7,14 @@
 $("#myw_button1").click(function(){
   var n1 = $("#myw_in1").val();
   if (n1==20){
-    window.open("https://www.cdfa.ca.gov/Statistics/PDFs/2020_Ag_Stats_Review.pdf");
-  }
-else if (n1==19) {
-    window.open("https://www.cdfa.ca.gov/statistics/PDFs/2018-2019AgReportnass.pdf");
+    window.open("https://www.cdfa.ca.gov/Statistics/PDFs/2020_Exports_Publication.pdf");
+  }else if (n1==19) {
+    window.open("https://www.cdfa.ca.gov/Statistics/PDFs/AgExports2018-2019.pdf");
   }else if (n1==18){
-    window.open("https://www.cdfa.ca.gov/Statistics/PDFs/2017-18AgReport.pdf");
+    window.open("https://www.cdfa.ca.gov/Statistics/PDFs/2017-18AgExports.pdf");
   }else if (n1==17){
     window.open("https://www.cdfa.ca.gov/Statistics/PDFs/2017AgExports.pdf");
   }else if (n1==16){
-    window.open("https://www.cdfa.ca.gov/Statistics/PDFs/2016Report.pdf");
+    window.open("https://www.cdfa.ca.gov/Statistics/PDFs/AgExports2015-2016.pdf");
   }
 })
