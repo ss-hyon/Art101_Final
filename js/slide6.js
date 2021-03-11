25ac58c864e176954a1ea223110ec59031743f01
ss-hyon <so.young.ee197@gmail.com>
2021-03-11 11:29:46 -0800

    3/11

--- js/slide6.js
+++ js/slide6.js
@@ -5,14 +5,27 @@
  * Public Domain
  **/
  //text box info
-$("#mk").hide();
-
+$("#mkHidden").hide();
+function searchFound(){
+   alert("YOU FOUND IT!");
+  window.open("https://ecologycenter.org/fmfinder/");
+}
  function marketInfo() {
-  var x = document.getElementById("mk");
-  $("x").hide();
+  var x = document.getElementById("mkHidden");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
 }
+var button = document.getElementById("mkAudio");
+var audio = document.getElementById("mkPlayer");
+button.addEventListener("click", function(){
+  if(audio.paused){
+    audio.play();
+    button.innerHTML = "Pause";
+  } else {
+    audio.pause();
+    button.innerHTML = "Play";
+  }
+});
